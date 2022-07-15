bbfa5cf3cbba2156bcf6226f70497a0fdd07e3bf
yuy <826298031@qq.com>
2022-07-15 19:56:48 +0800

    邮件功能

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommunityApplication {
-	//1
+	//12
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
 	}
